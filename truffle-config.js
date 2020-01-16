0d1fb4057ec68f14e6f6069838cae0ce04eac876
fainashalts <faina@trufflesuite.com>
2020-01-16 08:58:55 -0800

    Test project

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  // Uncommenting the defaults below 
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks;
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  //
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+   xdevelopment: {
+      host: "127.0.0.1",
+      port: 8546,
+      network_id: "*"
+    },
+    secondary: {
+      host: "127.0.0.1",
+      port: 8546,
+      network_id: "*"
+    },
+  },
+  db: {
+    enable: {
+      saveAfter: {
+        compile: true,
+        migrate: true
+      }
+    }
+  }
+};
